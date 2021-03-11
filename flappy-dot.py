a633f9badafaa127437bbda93df14be41099a1bd
Thanabardi Tashasiraphat <thanabardi@hotmail.com>
2021-03-11 17:41:23 +0700

    dot create

--- flappy-dot.py
+++ flappy-dot.py
@@ -1,75 +1,61 @@
 import tkinter as tk
-import random
-from gamelib import *
+
+from gamelib import Sprite, GameApp, Text
 
 CANVAS_WIDTH = 800
 CANVAS_HEIGHT = 500
 
 UPDATE_DELAY = 33
-PILLAR_SPEED = 5
+GRAVITY = 2.5
+STARTING_VELOCITY = -30
+JUMP_VELOCITY = -20
 
-class PillarPair():
-    def __init__(self, game_app, speed, expand_x=0, color="green"):
-        self.canvas = game_app.canvas
-        self.canvas_height = game_app.canvas_height
-        self.canvas_width = game_app.canvas_width
-        self.color = color
-        self.speed = speed
-        self.expand_x = expand_x
-
-        self.create_pillars()
-        
+class Dot(Sprite):
+    def init_element(self):
+        self.vy = STARTING_VELOCITY
+        self.is_started = False
+    
     def update(self):
-        self.upper_pillar.x -= self.speed
-        self.lower_pillar.x -= self.speed
-        if self.upper_pillar.x + self.upper_pillar.size_x < 0:
-            self.reset()
+        if self.is_started:
+            self.y += self.vy
+            self.vy += GRAVITY
+    
+    def start(self):
+        self.is_started = True
 
-    def render(self):
-        self.upper_pillar.render()
-        self.lower_pillar.render()
+    def jump(self):
+        self.vy = JUMP_VELOCITY
 
-    def create_pillars(self):
-        self.set_pillars_info()
-        self.upper_pillar = Contour(self, "r", color="blue", x=self.canvas_width+self.width+self.expand_x, y=self.upper_y, size_x=self.width, size_y=self.upper_height)
-        self.lower_pillar = Contour(self, "r", color="red", x=self.canvas_width+self.width+self.expand_x, y=self.lower_y, size_x=self.width, size_y=self.lower_height)
+class FlappyGame(GameApp):
+    def create_sprites(self):
+        self.dot = Dot(self, 'images/dot.png', CANVAS_WIDTH // 6, CANVAS_HEIGHT // 2)
 
-    def set_pillars_info(self):
-        rand_point = random.randint(100, self.canvas_height-100)
-        space = 100
-        self.width = self.canvas_width / 10
+        self.elements.append(self.dot)
 
-        self.upper_height = rand_point - space/2
-        self.upper_y = self.upper_height/2
-
-        self.lower_height = self.canvas_height - rand_point - space/2
-        self.lower_y = self.canvas_height - self.lower_height/2
-
-    def reset(self):
-        self.set_pillars_info()
-        self.upper_pillar.set_shape(self.canvas_width+self.width, self.upper_y, self.width, self.upper_height)
-        self.lower_pillar.set_shape(self.canvas_width+self.width, self.lower_y, self.width, self.lower_height)
-
-    def get_hitbox(self):
-        return self.upper_pillar.get_hitbox(), self.lower_pillar.get_hitbox()
-
-class FlappyDot(GameApp):
     def init_game(self):
-        self.create_pillars()
+        self.create_sprites()
 
-    def create_pillars(self):
-        for i in range(5):
-            self.elements.append(PillarPair(self, PILLAR_SPEED, expand_x=i*190))
+    def pre_update(self):
+        pass
+
+    def post_update(self):
+        pass
+
+    def on_key_pressed(self, event):
+        if event.char == ' ':
+            if not self.dot.is_started:
+                self.dot.start()
+            else:
+                self.dot.jump()
+                self.dot.update()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.resizable(0, 0)
-
-    app = FlappyDot(root, CANVAS_WIDTH, CANVAS_HEIGHT)
+    root.title("Monkey Banana Game")
+ 
+    # do not allow window resizing
+    root.resizable(False, False)
+    app = FlappyGame(root, CANVAS_WIDTH, CANVAS_HEIGHT, UPDATE_DELAY)
     app.start()
-
-    # print(app.circle.get_hitbox())
-    # print(app.rectangle.get_hitbox())
-    # print(app.test_text.text)
-
     root.mainloop()
--- gamelib.py
+++ gamelib.py
@@ -1,49 +1,7 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
-class GameApp(ttk.Frame):
-    def __init__(self, parent, canvas_width=800, canvas_height=500, update_delay=33):
-        super().__init__(parent)
-        
-        # initial constant
-        self.canvas_width = canvas_width
-        self.canvas_height = canvas_height
-        self.update_delay = update_delay
-        self.parent = parent
-        self.grid(sticky="nsew")
-        # create and setup canvas
-        self.create_canvas()
-        # game_elements setting
-        self.elements = []
-        self.init_game()
-        # keyboard-event binding
-        self.parent.bind("<KeyPress>", self.on_key_pressed)
-        self.parent.bind("<KeyRelease>", self.on_key_released)
-
-    def create_canvas(self):
-        self.canvas = tk.Canvas(self, borderwidth=0, width=self.canvas_width, height=self.canvas_height)
-        self.canvas.grid(sticky="nsew")
-        
-    def animate(self):
-        for elem in self.elements:
-            elem.update()
-            elem.render()
-        self.after_id = self.after(self.update_delay, self.animate)
-
-    def start(self):
-        self.after_id = self.after(0, self.animate)
-
-    def init_game(self):
-        # game init setup for sub-class
-        pass
-
-    def on_key_pressed(self, event):
-        pass
-
-    def on_key_released(self, event):
-        pass
-
-class GameElement():
+class GameCanvasElement():
     def __init__(self, game_app, x=0, y=0):
         self.x = x
         self.y = y
@@ -56,77 +14,104 @@ class GameElement():
 
     def show(self):
         self.is_visible = True
-        self.canvas.itemconfigure(self.object_id, state="normal")
+        self.canvas.itemconfigure(self.canvas_object_id, state="normal")
 
     def hide(self):
         self.is_visible = False
-        self.canvas.itemconfigure(self.object_id, state="hiden")
+        self.canvas.itemconfigure(self.canvas_object_id, state="hidden")
+
+    def render(self):
+        if self.is_visible:
+            self.canvas.coords(self.canvas_object_id, self.x, self.y)
+
+    def init_canvas_object(self):
+        pass
+
+    def init_element(self):
+        pass
 
     def update(self):
         pass
 
-    def render(self):
-        if self.is_visible:
-            self.canvas.coords(self.object_id, self.x, self.y)
-
-    def init_canvas_object(self):
-        """ init canvas object """
-        pass
-
-    def init_element(self):
-        """ init element properties """
-        pass
-
-class Text(GameElement):
+class Text(GameCanvasElement):
     def __init__(self, game_app, text, x=0, y=0):
         self.text = text
         super().__init__(game_app, x, y)
-        
+
     def init_canvas_object(self):
-        self.object_id = self.canvas.create_text(self.x, self.y, text=self.text)
+        self.canvas_object_id = self.canvas.create_text(
+            self.x, 
+            self.y,
+            text=self.text)
 
     def set_text(self, text):
         self.text = text
-        self.canvas.itemconfigure(self.object_id, text=self.text)
+        self.canvas.itemconfigure(self.canvas_object_id, text=text)
+        
 
-class Contour(GameElement):
-    def __init__(self, game_app, shape, color="blue", x=0, y=0, size_x=5, size_y=5):
-        self.shape = shape
-        self.size_x = size_x
-        self.size_y = size_y
-        self.color = color
+class Sprite(GameCanvasElement):
+    def __init__(self, game_app, image_filename, x=0, y=0):
+        self.image_filename = image_filename
         super().__init__(game_app, x, y)
 
     def init_canvas_object(self):
-        if self.shape == "c" or self.shape == "circle":
-            self.object_id = self.canvas.create_oval(self.x-self.size_x/2, 
-                                                     self.y-self.size_y/2,
-                                                     self.x+self.size_x/2,
-                                                     self.y+self.size_y/2,
-                                                     fill=self.color)
-                                                     
-        elif self.shape == "r" or self.shape == "rectangle":
-            self.object_id = self.canvas.create_rectangle(self.x-self.size_x/2, 
-                                                          self.y-self.size_y/2,
-                                                          self.x+self.size_x/2,
-                                                          self.y+self.size_y/2,
-                                                          fill=self.color)
+        self.photo_image = tk.PhotoImage(file=self.image_filename)
+        self.canvas_object_id = self.canvas.create_image(
+            self.x, 
+            self.y,
+            image=self.photo_image)
 
-        else:
-            raise TypeError(f"{self.shape} is not included")
 
-    def render(self):
-        self.canvas.coords(self.object_id,
-                           self.x-self.size_x/2, 
-                           self.y-self.size_y/2,
-                           self.x+self.size_x/2,
-                           self.y+self.size_y/2)
+class GameApp(ttk.Frame): 
+    def __init__(self, parent, canvas_width=800, canvas_height=500, update_delay=33):
+        super().__init__(parent)
+        self.parent = parent
+        
+        self.canvas_width = canvas_width
+        self.canvas_height = canvas_height
+        
+        self.update_delay = update_delay
 
-    def set_shape(self, x, y, size_x, size_y):
-        self.x = x
-        self.y = y
-        self.size_x = size_x
-        self.size_y = size_y
+        self.grid(sticky="news")
+        self.create_canvas()
 
-    def get_hitbox(self):
-        return (self.x-self.size_x/2, self.y-self.size_y/2, self.x+self.size_x/2, self.size_y+self.size_y)
+        self.elements = []
+        self.init_game()
+
+        self.parent.bind('<KeyPress>', self.on_key_pressed)
+        self.parent.bind('<KeyRelease>', self.on_key_released)
+        
+    def create_canvas(self):
+        self.canvas = tk.Canvas(self, borderwidth=0,
+            width=self.canvas_width, height=self.canvas_height, 
+            highlightthickness=0)
+        self.canvas.grid(sticky="news")
+
+    def animate(self):
+        self.pre_update()
+
+        for element in self.elements:
+            element.update()
+            element.render()
+
+        self.post_update()
+
+        self.after(self.update_delay, self.animate)
+
+    def start(self):
+        self.after(0, self.animate)
+
+    def init_game(self):
+        pass
+
+    def pre_update(self):
+        pass
+
+    def post_update(self):
+        pass
+
+    def on_key_pressed(self, event):
+        pass
+
+    def on_key_released(self, event):
+        pass
